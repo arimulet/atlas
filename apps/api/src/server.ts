ff5d5a65dd7a100b5ebf65e7c38497e3e4a4ad59
Ariel Mulet <mulet.ariel@gmail.com>
2026-08-05 20:01:42 -0300

    Configure API dotenv environment

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import Fastify from "fastify";
 import { importPlayerSnapshotMvp, validatePlayerSnapshotImport } from "@atlas/application";
 import { connectMongoDb } from "@atlas/database";
