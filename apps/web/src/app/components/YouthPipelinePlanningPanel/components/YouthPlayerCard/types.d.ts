517c9f7bf112a928264e3a58711461ea87492572
Ariel Mulet <mulet.ariel@gmail.com>
2026-08-06 21:50:08 -0300

    fix types

--- apps/web/src/app/components/YouthPipelinePlanningPanel/components/YouthPlayerCard/types.d.ts
+++ apps/web/src/app/components/YouthPipelinePlanningPanel/components/YouthPlayerCard/types.d.ts
@@ -1,4 +1,4 @@
-import type { YouthPipelinePlayerPlan } from "../../types";
+import type { YouthPipelinePlayerPlan } from "../../../../types";
 
 export interface YouthPlayerCardProps {
   player: YouthPipelinePlayerPlan;
