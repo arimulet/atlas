248ca6cf667b70faca217d05d4ad4dc4cca9d49a
Ariel Mulet <mulet.ariel@gmail.com>
2026-08-07 16:31:56 -0300

    refactor api typing (#47)

---
+++ apps/api/src/routes/club/types.d.ts
@@ -0,0 +1,10 @@
+import { ClubParams } from "../../types.js";
+
+export type GetClubProfileParams = ClubParams;
+export type GetClubDashboardParams = ClubParams;
+export type PatchClubProfileParams = ClubParams;
+export type GetClubOperatingSettingsParams = ClubParams;
+export type PatchClubOperatingSettingsParams = ClubParams;
+export type GetClubHistoricalTrendsParams = ClubParams;
+export type GetClubHistoricalFindingsParams = ClubParams;
+export type GetClubSnapshotsParams = ClubParams;
---
+++ apps/api/src/routes/economy/types.d.ts
@@ -0,0 +1,4 @@
+import { ClubParams } from "../../types.js";
+
+export type GetEconomyParams = ClubParams;
+export type GetSquadMarketPlanningParams = ClubParams;
