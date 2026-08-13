f6a0a18d0f1ce154a843df510e03f0f95a708a7f
Ariel Mulet <mulet.ariel@gmail.com>
2026-08-13 01:50:24 -0300

    add generic types and constants for domain

---
+++ packages/domain/src/constants.ts
@@ -0,0 +1,12 @@
+import type { SkillKey } from "./types.js";
+
+export const SUPPORED_SKILLS: readonly SkillKey[] = [
+  "stamina",
+  "pace",
+  "technique",
+  "passing",
+  "keeper",
+  "defender",
+  "playmaker",
+  "striker"
+];
