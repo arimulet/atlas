e90dd6a7bcaf5662e02b3eaf5a2cc7afc90dfe33
Ariel Mulet <mulet.ariel@gmail.com>
2026-08-28 23:39:42 -0300

    add formation field for junior collection

---
+++ test-parser.js
@@ -0,0 +1,15 @@
+﻿import { parseJuniorsXml } from "./packages/application/src/importer/parsers/xml-juniors-parser.js";
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sokker>
+  <juniors>
+    <junior>
+      <id>5001</id>
+      <formation>0</formation>
+    </junior>
+    <junior>
+      <id>5002</id>
+      <formation>1</formation>
+    </junior>
+  </juniors>
+</sokker>`;
+console.log(parseJuniorsXml(xml));
