ed16095ebb35d0e67154c8f303b0d52e82e2c799
Ariel Mulet <mulet.ariel@gmail.com>
2026-09-05 14:58:42 -0300

    add optimization load and cache revalidate

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": {}
+  },
   resolve: {
     alias: {
       '@atlas/web': path.resolve(__dirname, './src') // Ajusta según la estructura de tu proyecto
